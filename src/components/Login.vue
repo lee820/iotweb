1051598bd8702a35ccc8f5e27fb967e4b6593ced
lee820 <476794594@qq.com>
2020-06-22 19:44:37 +0800

    login finish

---
+++ src/components/Login.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <h1>Login</h1>
+            <el-form ref="loginFormRef" label-width="80px" :model="LoginForm" :rules="rules" class="login_form">
+                <el-form-item label="用户名:" prop="username">
+                    <el-input v-model="LoginForm.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码:" prop="password">
+                    <el-input type="password" v-model="LoginForm.password"></el-input>
+                </el-form-item>
+                <el-form-item class="login_btns">
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      LoginForm: {
+        username: 'lee123',
+        password: '123456'
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 16, message: '长度在3到16个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '长度在6到16个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate((valid) => {
+        if (!valid) {
+          this.$message.error('登录失败')
+          return
+        }
+        const result = this.$http.post('login', this.LoginForm)
+        console.log(result)
+        this.$message.success('登录成功')
+        // 1. 将登录成功后的token，保存到客户端的sessionStorage中
+        //  1.1 项目除了登录之外的接口，都必须登录才能访问
+        //  1.2 token只在当前网站打开期间生效 sessionStorage打开生效。localsession场景存储
+        window.sessionStorage.setItem('token', result.data.token)
+        // 2. 通过编程式导航跳转home
+        // this.$router.pusho('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login_container {
+    background-color: #2d4d6d;
+    height: 100%;
+}
+
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #ffffff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.login_box h1 {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+}
+
+.login_btns {
+    display: flex;
+    float: right;
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    padding: 20px;
+    width: 100%;
+    box-sizing: border-box;
+}
+</style>
